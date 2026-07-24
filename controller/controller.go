7699c59806756ff7bab0687406de13facd03b2b2
PasarGuard Fork <dev@local>
2026-07-24 01:40:49 +0000

    controller: wire BackendType_SING_BOX dispatch to backend/singbox
    Final piece of the sing-box (Hysteria2) core integration - mirrors
    the existing xray/wireguard dispatch pattern in StartBackend.

--- controller/controller.go
+++ controller/controller.go
@@ -10,6 +10,7 @@ import (
 	"github.com/google/uuid"
 
 	"github.com/pasarguard/node/backend"
+	"github.com/pasarguard/node/backend/singbox"
 	"github.com/pasarguard/node/backend/wireguard"
 	"github.com/pasarguard/node/backend/xray"
 	"github.com/pasarguard/node/common"
@@ -145,6 +146,23 @@ func (c *Controller) StartBackend(ctx context.Context, backend *common.Backend) 
 			return err
 		}
 		newBackend, err := wireguard.New(c.cfg, config, backend.GetUsers())
+		if err != nil {
+			return err
+		}
+		c.backend = newBackend
+
+	case common.BackendType_SING_BOX:
+		config, err := singbox.NewConfig(backend.GetConfig())
+		if err != nil {
+			return err
+		}
+		newBackend, err := singbox.New(
+			ctx,
+			config,
+			backend.GetUsers(),
+			c.apiPort,
+			c.cfg,
+		)
 		if err != nil {
 			return err
 		}
